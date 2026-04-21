83e1e5c15655f8cb1aa0094ef334f1c8b489c6ef
jesus david <jesusdavidbecerralopez69@gmail.com>
2026-04-20 19:24:42 -0500

    feat: Archivos de todos lo casos java listos

---
+++ Contenido-java/Caso16.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Caso16 {
+
+  public static void main(String[] args) {
+
+    Scanner jm = new Scanner(System.in);
+
+    // Caso 16. Bibliotecología: Estadísticas de Circulación de Textos Una biblioteca registra el número de libros prestados diariamente durante un mes de 24 días hábiles. Los datos se almacenan en un vector. El sistema debe identificar los "Días de Baja Afluencia" (menos de 10 libros) y calcular el total de circulación mensual para planeación presupuestal.
+
+    System.out.println("\n===== Inicio del sistema de estadísticas de Circulación de Textos=====");
+
+
+
+  }
+}
